148cbd562ba6dd502ddb040525867588a6222b04
sergiofernandez65 <sergio10fernandezzz@gmail.com>
2025-12-05 13:48:49 +0100

    Add files via upload

---
+++ 4/prueba1.js
@@ -0,0 +1,15 @@
+
+           // Soy un comentario de una linea
+            /*  j-------------- */
+            //alert("Prueba de codigo")
+            var mensaje = "hola mundo";
+            console.log(mensaje);
+            console.log(mensaje);
+            console.log(document);
+            console.log(localStorage);
+            var esviernes =true;
+            console.log("Es viernes y queremos fiesta"+ esviernes);
+            //debugger
+            if(esviernes == true){
+                console.log("Pues si que es viernes, me voy a emborrachar");
+            }
